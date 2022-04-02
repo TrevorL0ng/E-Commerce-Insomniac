ea504cf769bae9816700eb8847374a9a006b3015
Trevor L <trevorlong992@gmail.com>
2022-04-02 14:06:18 -0400

    Mostly done with the last route. Snacc and mental break needed. Be back in a few

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,29 +5,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({include: [{model:Category}, {model:Tag, through:ProductTag}]})
+    .then(productData => res.json(productData))
+    .catch(err => {console.log(err);
+           res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne ({
+    where: {id: req.params.id},
+    include: [{model:Category}]    
+  })
+    .then(productData => res.json(productData))
+    .catch(err => {console.log(err);
+           res.status(500).json(err);
+  });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create()
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
