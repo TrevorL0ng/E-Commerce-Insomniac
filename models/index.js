5935cc711cb3829968d7e1071b510cfd94e0b84f
Trevor L <trevorlong992@gmail.com>
2022-04-03 22:31:50 -0400

    All done, time for turn in

--- models/index.js
+++ models/index.js
@@ -4,13 +4,12 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Setting up associations
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category);
+Category.hasMany(Product);
+Product.belongsToMany(Tag, {through:ProductTag});
+Tag.belongsToMany(Product, {through:ProductTag});
 
 module.exports = {
   Product,
